91adf6ad54d9996872ea3d9ee0f4a3c72155af67
Carlos Hernandez <carloshdmn@gmail.com>
2016-03-15 15:04:13 +0100

    document ready -> device ready

--- js/script.js
+++ js/script.js
@@ -1,43 +1,46 @@
 
-$(document).ready(function() {
+// $(document).ready(function() {
+document.addEventListener("deviceready", function() {
 
-    $('#register').on('click', function() {
-        // jeje
-        console.log('no hago nada');
-    })
+	// $('#register').on('click', function() {
+	//     // jeje
+	//     console.log('no hago nada');
+	// })
 
-    var push = PushNotification.init({
-        android: {
-            senderID: "214060846526"
-        },
-        ios: {
-            alert: "true",
-            badge: "true",
-            sound: "true"
-        },
-        windows: {}
-    });
+	var push = PushNotification.init({
+		android: {
+			senderID: "214060846526"
+		},
+		ios: {
+			alert: "true",
+			badge: "true",
+			sound: "true"
+		},
+		windows: {}
+	});
 
-    push.on('registration', function(data) {
-        // data.registrationId
-        alert(data);
-        alert(data.registrationId);
-        $("#gcm_id").html(data.registrationId);
-    });
+	push.on('registration', function(data) {
+		// data.registrationId
+		alert(data);
+		alert(data.registrationId);
+		$("#gcm_id").html(data.registrationId);
+	});
 
-    push.on('notification', function(data) {
-        // data.message,
-        // data.title,
-        // data.count,
-        // data.sound,
-        // data.image,
-        // data.additionalData
-        $("#notifications").prepend('<li> ' + data.message + ' : ' + data.message + ' - (' + data.count + ') img: ' + data.image + '. additionalData: ' + data.additionalData + '</li>');
-    });
+	push.on('notification', function(data) {
+		// data.message,
+		// data.title,
+		// data.count,
+		// data.sound,
+		// data.image,
+		// data.additionalData
+		$("#notifications").prepend('<li> ' + data.message + ' : ' + data.message + ' - (' + data.count + ') img: ' + data.image + '. additionalData: ' + data.additionalData + '</li>');
+	});
 
-    push.on('error', function(e) {
-        // e.message
-        $("#notifications").prepend('<li class="error"> ' + e.message + '</li>');
-    });
+	push.on('error', function(e) {
+		// e.message
+		$("#notifications").prepend('<li class="error"> ' + e.message + '</li>');
+	});
+	
+}, false);
 
-});
+// });
