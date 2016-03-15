ddab64186ec285259eac66d5a06336992b5aa7c3
Carlos Hernandez <carloshdmn@gmail.com>
2016-03-15 13:27:43 +0100

    On ready...

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-var push = PushNotification.init({
-    android: {
-        senderID: "214060846526"
-    },
-    ios: {
-        alert: "true",
-        badge: "true",
-        sound: "true"
-    },
-    windows: {}
-});
 
-push.on('registration', function(data) {
-    // data.registrationId
-    $("#gcm_id").html(data.registrationId);
-});
+$(document).ready(function() {
 
-push.on('notification', function(data) {
-    // data.message,
-    // data.title,
-    // data.count,
-    // data.sound,
-    // data.image,
-    // data.additionalData
-    $("#notifications").prepend('<li> ' + data.message + ' : ' + data.message + ' - (' + data.count + ') img: ' + data.image + '. additionalData: ' + data.additionalData + '</li>');
-});
+    $('#register').on('click', function() {
+        // jeje
+        console.log('no hago nada');
+    })
 
-push.on('error', function(e) {
-    // e.message
-    $("#notifications").prepend('<li class="error"> ' + e.message + '</li>');
+    var push = PushNotification.init({
+        android: {
+            senderID: "214060846526"
+        },
+        ios: {
+            alert: "true",
+            badge: "true",
+            sound: "true"
+        },
+        windows: {}
+    });
+
+    push.on('registration', function(data) {
+        // data.registrationId
+        $("#gcm_id").html(data.registrationId);
+    });
+
+    push.on('notification', function(data) {
+        // data.message,
+        // data.title,
+        // data.count,
+        // data.sound,
+        // data.image,
+        // data.additionalData
+        $("#notifications").prepend('<li> ' + data.message + ' : ' + data.message + ' - (' + data.count + ') img: ' + data.image + '. additionalData: ' + data.additionalData + '</li>');
+    });
+
+    push.on('error', function(e) {
+        // e.message
+        $("#notifications").prepend('<li class="error"> ' + e.message + '</li>');
+    });
+
 });
